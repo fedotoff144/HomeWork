e10f765933e0cccc2c48872698e940aed519c256
fedotoff144 <fedotoff144@gmail.com>
2022-09-08 21:14:58 +0300

    Creat repository

---
+++ EnteringProgramming/Task_019/Program.cs
@@ -0,0 +1,13 @@
+﻿// A Task - 19
+
+Console.Write("Введите число: ");
+string? num = Console.ReadLine();
+char[] numArray = num.ToCharArray();
+Array.Reverse(numArray);
+string reverse = new string(numArray);
+
+if (reverse == num){
+    Console.WriteLine("Полиандром!");
+} else {
+    Console.WriteLine("Не полиандром!");
+}
---
+++ EnteringProgramming/Task_023/Program.cs
@@ -0,0 +1,7 @@
+﻿// A Task - 23
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++){
+    Console.WriteLine(i + "³ = " + i * i * i);
+}
