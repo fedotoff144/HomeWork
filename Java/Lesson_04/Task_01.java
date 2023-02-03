10b737e8108e41bdcd8ffa500f4b9f39358debe2
fedotoff144 <fedotoff144@gmail.com>
2023-02-03 14:08:56 +0300

    add homework for 4 lesson

---
+++ Java/Lesson_04/Task_01.java
@@ -0,0 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class Task_03 {
+    public static void main(String[] args) {
+        // Дан Deque состоящий из последовательности цифр.
+        // Необходимо проверить, что последовательность цифр является палиндромом
+
+        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1, 2, 3, 2, 1, 0));
+        System.out.println(checkOn(deque));
+    }
+
+    public static boolean checkOn(Deque<Integer> deque) {
+        while (deque.size() > 1) {
+            if (deque.pollFirst() != deque.pollLast())
+                break;
+        }
+        if (deque.size() > 1)
+            return false;
+        return true;
+    }
+}
