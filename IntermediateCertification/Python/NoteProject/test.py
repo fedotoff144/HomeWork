c4dc4da8fb04fef73d50eab24c234ebf8a01a099
fedotoff144 <fedotoff144@gmail.com>
2023-04-06 09:47:56 +0300

    changes file test

--- IntermediateCertification/Python/NoteProject/test.py
+++ IntermediateCertification/Python/NoteProject/test.py
@@ -1,4 +1,5 @@
 import io
+import re
 
 
 # def read_all_notes():
@@ -6,12 +7,43 @@ import io
 #         data.read()
 
 
-# a = '140162872638912'
+a = '140162872638912'
 #
 # # if a in read_all_notes():
 # #     print('ok')
 #
-# with io.open('Notes.csv', encoding='utf-8') as file:
-#     for line in file:
-#         if a in line:
-#             print('ok')
+with io.open('Notes.csv', encoding='utf-8') as file:
+    for line in file:
+        if a in line:
+            print(line)
+
+
+
+with open('f.txt') as f:
+    lines = f.readlines()
+
+str = 'Hello'
+pattern = re.compile(re.escape(str))
+with open('f.txt', 'w') as f:
+    for line in lines:
+        result = pattern.search(line)
+        if result is None:
+            f.write(line)
+
+# Или почти тоже самое, но в один проход (но я за первый вариант)
+#
+# #!/usr/bin/env python3
+
+import re
+
+str = 'Hello'
+pattern = re.compile(re.escape(str))
+with open('f.txt', 'r+') as f:
+    lines = f.readlines()
+    f.seek(0)
+    for line in lines:
+        result = pattern.search(line)
+        if result is None:
+            f.write(line)
+        f.truncate()
+
