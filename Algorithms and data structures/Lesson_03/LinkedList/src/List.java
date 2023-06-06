7264a0b4ec91557a836dc723428b052d9bb08290
fedotoff144 <fedotoff144@gmail.com>
2023-06-06 23:34:44 +0300

    method reverseList don't work, reason unknown

--- Algorithms and data structures/Lesson_03/LinkedList/src/List.java
+++ Algorithms and data structures/Lesson_03/LinkedList/src/List.java
@@ -8,32 +8,48 @@ public class List {
             Node prev;
         }
 
-        public void add(int value) {
+        public void addToTop(int value) {
             Node newNode = new Node();
             newNode.value = value;
             newNode.next = head;
             head = newNode;
         }
 
-        public void reverse() {
-            Node currentNode=head;
+        public void addToEnd(int value) {
+            Node newNode = new Node();
+            newNode.value = value;
+            Node currentNode = head;
+
+            if (currentNode != null) {
+                while (currentNode.next != null) {
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = newNode;
+            } else {
+                head = newNode;
+            }
+        }
+
+        public void reverseList() {
+            Node currentNode = head;
             currentNode.prev = null;
 
-            while (currentNode != null){
+            while (currentNode.next != null) {
                 Node temp = currentNode.next;
-                currentNode.next=currentNode.prev;
-                currentNode.prev=currentNode;
-                currentNode=temp;
+                currentNode.next = currentNode.prev;
+                currentNode.prev = currentNode;
+                currentNode = temp;
+
             }
         }
 
         public void print() {
             Node currentNode = head;
             if (head != null) {
-                System.out.println(head.value);
+                System.out.print(head.value + " ");
                 while (currentNode.next != null) {
                     currentNode = currentNode.next;
-                    System.out.println(currentNode.value);
+                    System.out.print(currentNode.value + " ");
                 }
             }
         }
@@ -42,16 +58,16 @@ public class List {
         public static void main(String[] args) {
             sList list = new sList();
 
-            list.add(1);
-            list.add(2);
-            list.add(3);
+            list.addToTop(1);
+            list.addToTop(2);
+            list.addToTop(3);
+            list.addToTop(4);            list.print();
 
+            System.out.println("\n------");
+
+            list.reverseList();
             list.print();
 
-            list.reverse();
-            System.out.println("----------------------------------");
-
-            list.print();
         }
 
     }
