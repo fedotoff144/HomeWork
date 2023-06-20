24bdd23be54eb730e2357bdd3230f0cf57cb7a96
fedotoff144 <fedotoff144@gmail.com>
2023-06-20 22:55:46 +0300

    all do again

--- Exceptions in programming/Lesson_03/src/Model/Validation.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public interface Validation {
-    boolean isValidGender(String data);
-    boolean isPhoneNumber(String data);
-    boolean isValidateDate(String data);
-    boolean isValidName(String data);
-
-}
