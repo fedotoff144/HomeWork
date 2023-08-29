34d793b74d70d1e4722fc37de3ea1a2f1d1062e3
fedotoff144 <fedotoff144@gmail.com>
2023-08-29 23:35:37 +0300

    work on homework 7 python

--- Dive into python/Lesson_07/Task_02.py
+++ Dive into python/Lesson_07/Task_02.py
@@ -15,4 +15,31 @@
 rename(wanted_name = "video", count_nums=3, extension_old=".txt", extension_new=".csv", diapazon=[3, 6])
 foto_2002.txt -> o_20video001.csv
 """
+import os
+from pathlib import Path
+
+
+COUNT: int = 0
+name = 'video'
+count_nums = 3
+extension_old = '.txt'
+extension_new = '.csv'
+diappazon = [3, 6]
+
+def rename(wanted_name: str, count_nums: int, extension_old: str, extension_new: str, diapazon: list):
+    files_list = os.listdir(os.getcwd())
+    for file in files_list:
+        if extension_old in file:
+            temp = file.split('.')
+            temp[0] = wanted_name + extension_new
+    return None
+
+
+files_list = os.listdir(os.getcwd())
+for file in files_list:
+    if extension_old in file:
+        COUNT += 1
+        file = f'{name}{COUNT}{extension_new}'
+
+
 
