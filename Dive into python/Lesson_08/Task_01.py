340ed28e93fd7b12ee362b09c4f0da6e718979c3
fedotoff144 <fedotoff144@gmail.com>
2023-09-04 23:49:46 +0300

    work on 1 task lesson 8 python

--- Dive into python/Lesson_08/Task_01.py
+++ Dive into python/Lesson_08/Task_01.py
@@ -24,16 +24,46 @@ test/users/names.txt
 Решить задания, которые не успели решить на семинаре(6, 7)
 """
 import os
+import csv
+import json
+import pickle
+
+path = os.getcwd()
 
 
-# def scan_function(path: str):
-#     files_list: list = os.walk(path)
-#
-#     return None
+def scan_files(path: str) -> list:
+    # сканируем все файлы по заданному пути
+    scan_list = list(os.walk(path))
+
+    # создаем пустой результирующий список который будет наполнен и возвращен функцией
+    res_list = []
+
+    for dir in scan_list:
+        # поочередно получаем папки с файлами из отсканированого списка
+        temp_list_files = os.listdir(dir[0])
+
+        # обходим папку и собираем информацию о каждом файле в ней
+        for file in temp_list_files:
+
+            # создаем временный словарь и заполняем его иниформацией о файле
+            temp_dict = {"name": file, "parent": dir[0].split('\\')[-1]}
+
+            # собираем полный путь к файлу для проверки принадлежности к файлу или к папке
+            full_path_file = os.path.join(dir[0], file)
+
+            if os.path.isdir(full_path_file):
+                temp_dict["type"] = "dir"
+            elif os.path.isfile(full_path_file):
+                temp_dict["type"] = "file"
+            else:
+                temp_dict["type"] = "unknown"
+
+            temp_dict["size"] = os.path.getsize(full_path_file)
+
+            # добавляем временный словарь с информацией о файле в результируюший список
+            res_list.append(temp_dict)
+
+    return res_list
 
 
-# print(os.getcwd())
-path: str = 'D:\GeekBrains\HomeWork\Dive into python'
-print(os.listdir(path))
-print(list(os.walk(os.listdir(path))))
-
+print(scan_files(path))
