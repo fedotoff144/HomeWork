2d7880596ad836bca057b7e994b180dac2701335
fedotoff144 <fedotoff144@gmail.com>
2023-09-11 16:52:39 +0300

    added task lesson 9 python

---
+++ Dive into python/Lesson_09/Task_01.py
@@ -0,0 +1,32 @@
+"""
+Напишите следующие функции:
+1. Нахождение корней квадратного уравнения
+2. Генерация csv файла с тремя случайными числами в каждой строке. 100-1000 строк.
+3. Декоратор, запускающий функцию нахождения корней квадратного уравнения
+   с каждой тройкой чисел из csv файла.
+4. Декоратор, сохраняющий переданные параметры и результаты работы функции в json файл.
+"""
+import csv
+import random
+
+FILE_SIZE = 100
+
+
+def find_equation_roots(a: int, b: int, c: int):
+    d = b ** 2 - (4 * a * c)
+    if d > 0:
+        x1 = (-b + (d ** 0.5)) / (2 * a)
+        x2 = (-b - (d ** 0.5)) / (2 * a)
+        x = [x1, x2]
+    elif d == 0:
+        x = -b / (2 * a)
+    else:
+        x = 'Корней нет'
+    return x
+
+
+def generate_csv_file():
+    with open('numbers.csv', 'w', newline='') as f:
+        for i in range(FILE_SIZE):
+            temp_int = random.randint(100, 999)
+            f.write(f'{temp_int}\n')
