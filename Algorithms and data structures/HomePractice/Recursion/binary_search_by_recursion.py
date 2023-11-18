76f1b9cc57b8e305596f645219a58adb58523460
fedotoff144 <fedotoff144@gmail.com>
2023-11-18 19:49:01 +0300

    finished binary search by recursion

--- Algorithms and data structures/HomePractice/Recursion/binary_search_by_recursion.py
+++ Algorithms and data structures/HomePractice/Recursion/binary_search_by_recursion.py
@@ -1,4 +1,4 @@
-my_arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+my_arr = [0,1,2,3,4,5,6]
 n = int(input("enter your number: "))
 min_index = 0
 max_index = len(my_arr)-1
@@ -9,14 +9,11 @@ def find_num(min_index:int, middle:int, max_index:int, arr:list):
   global n
   if n > arr[middle]:
     min_index = middle
-    middle = (max_index-middle)//2
-    print(middle)
+    middle = (max_index - min_index + 1)//2 + min_index
     return find_num(min_index, middle, max_index, arr)
   if n < arr[middle]:
     max_index = middle
-    print(f"{max_index=}")
-    middle = max_index//2
-    print(f"{middle=}")
+    middle = (max_index - min_index)//2 + min_index
     return find_num(min_index, middle, max_index, arr)
   return middle
 
