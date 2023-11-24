f0289237c0978681893be0008aa1e91b2196ec82
fedotoff144 <fedotoff144@gmail.com>
2023-11-24 14:32:33 +0300

    added algorithm quick sort

--- Algorithms and data structures/HomePractice/Recursion/quick_sort_by_recursion.py
+++ Algorithms and data structures/HomePractice/Recursion/quick_sort_by_recursion.py
@@ -1,1 +1,16 @@
 # quick sort by recursion
+
+my_arr = [6, 3, 4, 2, 5, 1, 0]
+
+
+def quick_sort(unsort_arr:list):
+  pivot = unsort_arr[0]
+  if len(unsort_arr) < 2:
+    return unsort_arr
+  else:
+    less_arr = [i for i in unsort_arr if i <= pivot]
+    greater_arr = [i for i in unsort_arr if i > pivot]
+    return quick_sort(less_arr+unsort_arr+greater_arr)
+    
+
+print(quick_sort(my_arr))
