f1d1fc4fdd42f1d20ca2a24ed1a95d9ff77576f2
fedotoff144 <fedotoff144@gmail.com>
2023-11-24 14:48:25 +0300

    edited quick sort

--- Algorithms and data structures/HomePractice/Recursion/quick_sort_by_recursion.py
+++ Algorithms and data structures/HomePractice/Recursion/quick_sort_by_recursion.py
@@ -3,13 +3,13 @@
 my_arr = [6, 3, 4, 2, 5, 1, 0]
 
 
-def quick_sort(unsort_arr:list):
+def quick_sort(unsort_arr):
   pivot = unsort_arr[0]
   if len(unsort_arr) < 2:
     return unsort_arr
   else:
-    less_arr = [i for i in unsort_arr if i <= pivot]
-    greater_arr = [i for i in unsort_arr if i > pivot]
+    less_arr = [i for i in unsort_arr if i[1] <= pivot]
+    greater_arr = [i for i in unsort_arr if i[1] > pivot]
     return quick_sort(less_arr+unsort_arr+greater_arr)
     
 
