d86444655f6dd2e972619d7996ce2ef374f608ee
fedotoff144 <fedotoff144@gmail.com>
2024-04-14 18:56:02 +0300

    Lesson_02 Task_022 solved

--- Dive into python/Lesson_02/Task_022.py
+++ Dive into python/Lesson_02/Task_022.py
@@ -10,4 +10,34 @@
 # Вывод:
 # 5/6 1/6
 
+import fractions
 
+first_fraction = input('Введите первую дробь в формате a/b: ')
+second_fraction = input('Введите вторую дробь в формате a/b: ')
+
+split_first_fraction = first_fraction.split('/')
+split_second_fraction = second_fraction.split('/')
+
+first_fraction_module = fractions.Fraction(first_fraction)
+second_fraction_module = fractions.Fraction(second_fraction)
+
+print('Решение:')
+
+# умножение дробей
+numerator = int(split_first_fraction[0]) * int(split_second_fraction[0])
+denominator = int(split_first_fraction[1]) * int(split_second_fraction[1])
+
+print(f'{first_fraction} * {second_fraction} = {numerator}/{denominator}')
+
+# сложение дробей
+numerator = (int(split_first_fraction[0]) * int(split_second_fraction[1]) +
+             int(split_first_fraction[1]) * int(split_second_fraction[0]))
+denominator = int(split_first_fraction[1]) * int(split_second_fraction[1])
+
+print(f'{first_fraction} + {second_fraction} = {numerator}/{denominator}')
+
+# решение с помощью модуля fractions
+print(f'Решение с помощью модуля fractions:')
+print(
+    f'{first_fraction} * {second_fraction} = {first_fraction_module * second_fraction_module}\n'
+    f'{first_fraction} + {second_fraction} = {first_fraction_module + second_fraction_module}')
