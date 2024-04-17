f4278e2a6a631097c167e205adb9cae8d92f5ca4
fedotoff144 <fedotoff144@gmail.com>
2024-04-17 10:48:01 +0300

    Task_033 solved

--- Dive into python/Lesson_03/Task_033.py
+++ Dive into python/Lesson_03/Task_033.py
@@ -11,14 +11,22 @@ my_stuff = {'rope': 3, 'pan': 5, 'tent': 10, 'plate': 1,
             'kettle': 3, 'pot': 4, 'blanket': 3, 'water': 5}
 result_list: list = []
 
-# Сортируем словарь с вещами и создаем список с возможными вариантами
+# Сортируем словарь с вещами по весу
 sorted_my_stuff = dict(sorted(my_stuff.items(), key=lambda x: x[1], reverse=True))
 
-for key, item in sorted_my_stuff.items():
-    if load_capacity - item > 0:
-        result_list.append(key)
-        load_capacity -= item
-        my_stuff.pop(key)
+# Запускаем цикл с добавлением элементов в список варианта с последующим добавлением варианта в результрующий список
+while len(my_stuff) > 0:
+    temp_list: list = []
+    temp_capacity = load_capacity
 
-print(my_stuff)
-print(result_list)
+    for key, item in sorted_my_stuff.items():
+        if temp_capacity - item >= 0 and key in my_stuff:
+            temp_list.append(key)
+            temp_capacity -= item
+            my_stuff.pop(key)
+    result_list.append(temp_list)
+
+# Выводим на печать результирующий список
+for i, _ in enumerate(result_list, 1):
+    print(f'Список {i}: {_}')
+
