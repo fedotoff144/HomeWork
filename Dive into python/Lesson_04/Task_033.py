2c6209a55a0be4228187b7c9f7a97e64e7230ea8
fedotoff144 <fedotoff144@gmail.com>
2024-04-22 16:15:01 +0300

    Task_033 in process of solving

--- Dive into python/Lesson_04/Task_033.py
+++ Dive into python/Lesson_04/Task_033.py
@@ -13,3 +13,67 @@
 Разбейте задачу на отдельные операции — функции. Дополнительно сохраняйте
 все операции поступления и снятия средств в список.
 """
+
+STEP = 50
+WITHDRAWAL_FEE = 0.015
+BONUS = 0.03
+UPPER_LIMIT = 600
+LOWER_LIMIT = 30
+MAX_AMOUNT = 5_000_000
+WEALTH_TAX = 0.1
+bank_account = 0
+count_operations: int = 0
+history = []
+
+
+def input_validation(mes):
+    while True:
+        try:
+            number = int(input(mes))
+            return number
+        except:
+            print('Ошибка. Попробуйте еще раз.')
+
+
+def refill():
+    global bank_account
+    global count_operations
+    amount = input_validation(f'Введите сумму пополнения кратную {STEP}у.е.: ')
+    if amount % STEP != 0:
+        refill()
+    else:
+        bank_account += amount
+        count_operations += 1
+
+
+def withdrawing_money():
+    amount = input_validation(f'Введите сумму снятия: ')
+
+
+def log_history(**kwargs):
+    global history
+    return history.append(kwargs)
+
+
+def read_history():
+    global history
+    for i in history:
+        print(i)
+
+
+def start_menu():
+    choice = input_validation('ВЫБЕРИТЕ MЕНЮ:\n1. Пополнить\n2. Снять\n3. Посмотреть историю\n0. Выйти\n')
+    match choice:
+        case 0:
+            exit('Пока!')
+        case 1:
+            refill()
+        case 2:
+            withdrawing_money()
+        case 3:
+            read_history()
+        case _:
+            print('Команда не найдена.')
+            start_menu()
+
+start_menu()
