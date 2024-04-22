4fbc5de0becd32c691776063050e7e1da42ca290
fedotoff144 <fedotoff144@gmail.com>
2024-04-22 14:20:24 +0300

    Task_022 solved

--- Dive into python/Lesson_04/Task_022.py
+++ Dive into python/Lesson_04/Task_022.py
@@ -6,3 +6,16 @@
 Пример: rev_kwargs(res=1, reverse=[1, 2, 3]) -> {1: 'res', '[1, 2, 3]': 'reverse'}
 
 """
+
+
+def convert_dict(**kwargs) -> dict:
+    result: dict = {}
+    for key, value in kwargs.items():
+        if not value.__hash__:
+            result[str(value)] = key
+        else:
+            result[value] = key
+    return result
+
+
+print(convert_dict(res=1, reverse=[1, 2, 3]))
