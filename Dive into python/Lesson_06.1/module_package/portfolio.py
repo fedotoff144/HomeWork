96ddf552303fc7af549b0928f7335528e116ad48
fedotoff144 <fedotoff144@gmail.com>
2024-04-24 22:42:11 +0300

    Task_01 in process of solving

---
+++ Dive into python/Lesson_06.1/module_package/portfolio.py
@@ -0,0 +1,13 @@
+__all__ = []
+
+
+def calculate_portfolio_value(stocks: dict, prices: dict) -> float:
+    return float()
+
+
+def calculate_portfolio_return(initial_value: float, current_value: float) -> float:
+    return float()
+
+
+def get_most_profitable_stock(stocks: dict, prices: dict) -> str:
+    return str()
