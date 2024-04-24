1fb167e7e2ef4a15b555330a5272e47b44069c43
fedotoff144 <fedotoff144@gmail.com>
2024-04-24 14:19:27 +0300

    Task_033 solved

--- Dive into python/Lesson_05/Task_033.py
+++ Dive into python/Lesson_05/Task_033.py
@@ -1,3 +1,25 @@
 """
 Создайте функцию генератор чисел Фибоначчи (см. Википедию)
 """
+
+
+def fib_gen(n):
+    a = 0
+    b = 1
+    for i in range(1, n + 1):
+        a, b = b, a + b
+        yield b
+
+
+iter_fib = iter(fib_gen(10))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib))
+print(next(iter_fib, 'stop generation'))
