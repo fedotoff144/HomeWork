ac5930a826298424f55e55375baabe7888b38498
fedotoff144 <fedotoff144@gmail.com>
2024-04-29 23:14:42 +0300

    Task_02 solved

--- Dive into python/Lesson_07.1/Task_02.py
+++ Dive into python/Lesson_07.1/Task_02.py
@@ -16,3 +16,24 @@ rename(wanted_name = "video", count_nums=3, extension_old=".csv", extension_new=
 foto_2002.txt -> o_20video001.csv
 
 """
+from pathlib import Path
+
+
+def rename_files(wanted_name: str, count_nums: int, extension_old: str,
+                 extension_new: str, diapazon: list):
+    pwd = Path.cwd()  # get home directory
+    count_files = 1  # counter changed files
+    slice_extention = len(extension_old)
+
+    for obj in pwd.iterdir():
+        if str(obj)[-slice_extention:] == extension_old:
+            count_str = '0' * (count_nums - len(str(count_files))) + \
+                        str(count_files)  # set counter to needed format
+            old_file_name = obj.name[diapazon[0]:diapazon[1]]  # get slice of old file name
+            new_file_name = old_file_name + wanted_name + count_str + \
+                            extension_new
+            obj.rename(new_file_name)
+            count_files += 1
+
+
+rename_files('_note_', 4, '.txt', '.txt', [0, 4])
