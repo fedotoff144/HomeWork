f0cb8cf540111187315e830e3cdedfb125673888
fedotoff144 <fedotoff144@gmail.com>
2024-05-02 08:43:37 +0300

    some changes Lesson_07.1

--- Dive into python/Lesson_07.1/Task_04.py
+++ Dive into python/Lesson_07.1/Task_04.py
@@ -7,15 +7,31 @@
 from pathlib import Path
 
 
-def sorting_files(home_dir):
-    for obj in home_dir.iterdir():
+def sorting_files(path):
+    home_dir = Path(path)
+
+    # create necessary folders for sorting
+    if not Path(home_dir / 'pictures').exists():
+        Path.mkdir(home_dir / 'pictures')
+    if not Path(home_dir / 'video').exists():
+        Path.mkdir(home_dir / 'video')
+    if not Path(home_dir / 'text').exists():
+        Path.mkdir(home_dir / 'text')
+    if not Path(home_dir / 'music').exists():
+        Path.mkdir(home_dir / 'music')
+
+    for obj in home_dir.rglob('*'):
         if obj.is_file():
-            if obj.name.__contains__('.jpg') or obj.name.__contains__('.png'):
+            if obj.name.endswith('.jpg') or obj.name.endswith('.png'):
                 obj.replace(home_dir / 'pictures' / obj.name)
-            if obj.name.__contains__('.mp4') or obj.name.__contains__('.avi'):
+            if obj.name.endswith('.mp4') or obj.name.endswith('.avi'):
                 obj.replace(home_dir / 'video' / obj.name)
-            if obj.name.__contains__('.txt') or obj.name.__contains__('.csv'):
+            if obj.name.endswith('.txt') or obj.name.endswith('.csv') \
+                    or obj.name.endswith('.doc') or obj.name.endswith('.docx') \
+                    or obj.name.endswith('.pdf'):
                 obj.replace(home_dir / 'text' / obj.name)
+            if obj.name.endswith('.mp3') or obj.name.endswith('.aac'):
+                obj.replace(home_dir / 'music' / obj.name)
 
 
-sorting_files(Path.cwd())
+sorting_files('H:\SamsungPhone')
--- Dive into python/Lesson_07.1/Task_05.py
+++ Dive into python/Lesson_07.1/Task_05.py
@@ -15,32 +15,35 @@
 """
 
 
-def mix_files(file_1, file_2):
+def mix_files(nums, names):
     with (
-        open(file_1, 'r', encoding='utf-8') as f1,
-        open(file_2, 'r', encoding='utf-8') as f2,
+        open(nums, 'r', encoding='utf-8') as f1,
+        open(names, 'r', encoding='utf-8') as f2,
         open('result.txt', 'a', encoding='utf-8') as f3
     ):
-        marker = 0  # marker for stop loop
+        marker_f1 = 0  # marker for stop loop
+        marker_f2 = 0  # marker for stop loop
         while True:
             line_f1 = f1.readline()
             line_f2 = f2.readline()
 
-            if not line_f1 or not line_f2 and marker == 1:
+            if not line_f1 and marker_f2 != 0 or not line_f2 and marker_f1 != 0:
                 break
-            if not line_f1 and marker == 0:
-                marker += 1
+            if not line_f1 and marker_f2 == 0:
+                marker_f1 += 1
                 f1.seek(0)
                 line_f1 = f1.readline()
-            if not line_f2 and marker == 0:
-                marker += 1
+            if not line_f2 and marker_f1 == 0:
+                marker_f2 += 1
                 f2.seek(0)
                 line_f2 = f2.readline()
 
+            # clear lines and perform calculations
             line_f1 = line_f1.replace('\n', '').split('|')
             line_f2 = line_f2.replace('\n', '')
             res_mult = int(line_f1[0]) * float(line_f1[1])
-            
+
+            # writing strings into file f3
             if res_mult <= 0:
                 line = f'{line_f2.lower()} {res_mult * (-1)}\n'
                 f3.write(line)
