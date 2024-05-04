a9ca1154118b89f73e919ff2030afd24120a2745
fedotoff144 <fedotoff144@gmail.com>
2024-05-04 23:11:12 +0300

    Task_01 solved

--- Dive into python/Lesson_08.1/modules/files_handling.py
+++ Dive into python/Lesson_08.1/modules/files_handling.py
@@ -31,13 +31,75 @@ from pathlib import Path
 __all__ = []
 
 
-def get_list_files(path: str):
+def get_list_files(path: str) -> list[dict]:
     home_dir = Path(path)
     structure_list = []
     for obj in home_dir.rglob('*'):
-        print(f'{obj.parts[-2]}\t{"File" if obj.is_file() else "Directory"}\t{obj=}')
+
+        # it is not necessary
+        if '.venv' in obj.parts:
+            continue
+
+        size_of_obj = obj.stat().st_size
+        if obj.is_dir():
+            for i in obj.rglob('*'):
+                size_of_obj += i.stat().st_size
+
+        temp_dict = {
+            'name': obj.name,
+            'parent': obj.parts[-2],
+            'type': 'file' if obj.is_file() else 'directory',
+            'size': size_of_obj
+        }
+        structure_list.append(temp_dict)
+    return structure_list
+
+
+def write_json_file(data: list):
+    with open('result.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+
+def read_json_file(file_name: str):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+
+def create_csv_file(data: list):
+    with open('result.csv', 'w', newline='', encoding='utf-8') as f:
+        csv_writer = csv.DictWriter(f, fieldnames=['name', 'parent', 'type', \
+                                                   'size'], dialect='excel',
+                                    quoting=csv.QUOTE_NONNUMERIC)
+        csv_writer.writeheader()
+        csv_writer.writerows(data)
+
+
+def read_csv_file(file_name: str) -> list:
+    with open(file_name, 'r', newline='', encoding='utf-8') as f:
+        f_read = csv.DictReader(f,
+                                fieldnames=['name', 'parent', 'type', 'size'],
+                                dialect='excel', quoting=csv.QUOTE_NONNUMERIC)
+        all_data = [line for i, line in enumerate(f_read) if i != 0]
+    return all_data
+
+
+def create_binary_file(data: list):
+    with open('result.pickle', 'wb') as f:
+        pickle.dump(data, f)
 
 
 if __name__ == '__main__':
     path_dir = 'D:\GeekBrains\HomeWork\Dive into python\Lesson_08.1'
-    get_list_files(path_dir)
+    list_of_structure = get_list_files(path_dir)
+
+    # write_json_file(data)
+    # output_json = read_json_file('result.json')
+    # print(output_json)
+
+    # create_csv_file(data)
+    # output_csv = read_csv_file('result.csv')
+    # print(output_csv)
+
+    print(list_of_structure)
+    create_binary_file(list_of_structure)
