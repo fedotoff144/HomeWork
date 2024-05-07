2bfe0dd59db743dc6aa5fbf092a6075a7385c1f8
fedotoff144 <fedotoff144@gmail.com>
2024-05-07 22:51:02 +0300

    Task_01 in process of solving

--- Dive into python/Lesson_09.1/Task_01.py
+++ Dive into python/Lesson_09.1/Task_01.py
@@ -7,35 +7,19 @@
 4. Декоратор, сохраняющий переданные параметры и результаты работы функции в json файл.
 """
 import csv
+import json
 import math
 import random
 from typing import Callable
 
 
-def csv_generation(num_rows: int, range_a: int, range_b: int):
-    with open('nums.csv', 'w', newline='', encoding='utf-8') as f:
-        write_line = csv.writer(f, dialect='excel',
-                                quoting=csv.QUOTE_NONNUMERIC)
-        for _ in range(num_rows):
-            row = random.choices(range(range_a, range_b), k=3)
-            write_line.writerow(row)
-
-
-def deco_find_roots(func: Callable):
-    with open('nums.csv', 'r', newline='', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            a, b, c = map(int, row)
-            result = func(a, b, c)
-            print(result)
-
-
-@deco_find_roots
-def math_function(a: int, b: int, c: int):
+def math_function(a, b, c):
+    """Calculate the roots of a square"""
     try:
         d = b ** 2 - 4 * a * c
         if d > 0:
-            x1, x2 = (-b - math.sqrt(d)) / (2 * a), (-b + math.sqrt(d)) / (2 * a)
+            x1, x2 = (-b - math.sqrt(d)) / (2 * a), (-b + math.sqrt(d)) / (
+                    2 * a)
             return x1, x2
         elif d == 0:
             x = -b / (2 * a)
@@ -46,4 +30,39 @@ def math_function(a: int, b: int, c: int):
         return 'Деление на 0'
 
 
-math_function()
+def csv_generation(num_rows: int, range_a: int, range_b: int):
+    with open('nums.csv', 'w', newline='', encoding='utf-8') as f:
+        write_line = csv.writer(f, dialect='excel',
+                                quoting=csv.QUOTE_NONNUMERIC)
+        for _ in range(num_rows):
+            row = random.sample(range(range_a, range_b), 3)
+            write_line.writerow(row)
+
+
+def deco_find_roots(func: Callable):
+    def wrapper():
+        all_data = []
+        with open('nums.csv', 'r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                a, b, c = map(int, row)
+                result = func(a, b, c)
+                all_data.append({'parameters': {'a': a, 'b': b, 'c': c},
+                                 'result': result})
+        return all_data
+    return wrapper
+
+
+def deco_logger(func: Callable):
+    def wrapper():
+        result = func()
+        with open('results.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+        return result
+    return wrapper
+
+
+@deco_find_roots
+@deco_logger
+def run_math():
+    return math_function()
