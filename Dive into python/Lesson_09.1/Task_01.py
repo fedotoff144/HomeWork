666d2333ca1f16615ac655c51d6daee12955c039
fedotoff144 <fedotoff144@gmail.com>
2024-05-08 14:28:05 +0300

    Task_01 solved

--- Dive into python/Lesson_09.1/Task_01.py
+++ Dive into python/Lesson_09.1/Task_01.py
@@ -13,8 +13,52 @@ import random
 from typing import Callable
 
 
+def csv_generation(range_a: int, range_b: int):
+    """generates the specified number of lines of random values
+    within the specified limits into a file.
+    """
+    num_rows = random.randint(100, 1000)
+    with open('nums.csv', 'w', newline='', encoding='utf-8') as f:
+        write_line = csv.writer(f, dialect='excel',
+                                quoting=csv.QUOTE_NONNUMERIC)
+        for _ in range(num_rows):
+            row = random.sample(range(range_a, range_b), 3)
+            write_line.writerow(row)
+
+
+def deco_find_roots(func: Callable):
+    """A decorator that runs a function to find the roots
+    of a quadratic equation with each triple of numbers from a csv file.
+    """
+    def wrapper():
+        with open('nums.csv', 'r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                a, b, c = map(int, row)
+                func(a, b, c)
+
+    return wrapper
+
+
+def deco_logger(func: Callable):
+    """A decorator that saves the passed parameters
+    and the results of the function to a json file.
+    """
+    def wrapper(*args):
+        result = func(*args)
+        record = {'parameters': f'a= {args[0]}, b= {args[1]}, c= {args[2]}',
+                  'result': result}
+        with open('results.json', 'a', encoding='utf-8') as f:
+            json.dump(record, f, ensure_ascii=False)
+
+    return wrapper
+
+
+# first way to decorate a mathematical function
+# @deco_find_roots
+# @deco_logger
 def math_function(a, b, c):
-    """Calculate the roots of a square"""
+    """Calculate the roots of a square."""
     try:
         d = b ** 2 - 4 * a * c
         if d > 0:
@@ -30,39 +74,8 @@ def math_function(a, b, c):
         return 'Деление на 0'
 
 
-def csv_generation(num_rows: int, range_a: int, range_b: int):
-    with open('nums.csv', 'w', newline='', encoding='utf-8') as f:
-        write_line = csv.writer(f, dialect='excel',
-                                quoting=csv.QUOTE_NONNUMERIC)
-        for _ in range(num_rows):
-            row = random.sample(range(range_a, range_b), 3)
-            write_line.writerow(row)
-
-
-def deco_find_roots(func: Callable):
-    def wrapper():
-        all_data = []
-        with open('nums.csv', 'r', newline='', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                a, b, c = map(int, row)
-                result = func(a, b, c)
-                all_data.append({'parameters': {'a': a, 'b': b, 'c': c},
-                                 'result': result})
-        return all_data
-    return wrapper
-
-
-def deco_logger(func: Callable):
-    def wrapper():
-        result = func()
-        with open('results.json', 'w', encoding='utf-8') as f:
-            json.dump(result, f, ensure_ascii=False, indent=4)
-        return result
-    return wrapper
-
-
-@deco_find_roots
-@deco_logger
-def run_math():
-    return math_function()
+csv_generation(-10, 10)
+# second way to decorate a mathematical function
+math_with_logger = deco_logger(math_function)
+whole_deco_math = deco_find_roots(math_with_logger)
+whole_deco_math()
