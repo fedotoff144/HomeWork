aa9aa5cfa8560beec25fd5144e5086664f0604c0
fedotoff144 PB laptop <fedotoff144@gmail.com>
2024-05-15 11:35:10 +0300

    Task_01 solved

--- Dive into python/Lesson_11.1/Task_01.py
+++ Dive into python/Lesson_11.1/Task_01.py
@@ -5,3 +5,18 @@
 📌 дополнительно хранятся имя автора строки и время создания
 (time.time)
 """
+import time
+
+
+class MyString(str):
+
+    def __new__(cls, value: str, author: str = 'unknown'):
+        instance = super().__new__(cls, value)
+        instance.author = author
+        instance.time = time.ctime(time.time())
+        return instance
+
+
+my_str = MyString('Some value of string', 'Pushkin A.S.')
+
+print(f'{my_str=}\t{my_str.author=}\t{my_str.time=}\t{type(my_str)}')
