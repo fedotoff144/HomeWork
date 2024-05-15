cb03b72cb4633cbb4250acc7fdee59d226ba2aaa
fedotoff144 GigabyteLaptop <fedotoff144@gmail.com>
2024-05-15 23:07:04 +0300

    Task_05 in process of solving

--- Dive into python/Lesson_11.1/Task_05.py
+++ Dive into python/Lesson_11.1/Task_05.py
@@ -7,3 +7,20 @@
 - Складываем и вычитаем периметры, а не длинну и ширину.
 - При вычитании не допускайте отрицательных значений.
 """
+
+
+class Rectangle:
+    def __init__(self, length, width=None):
+        self.length = length
+        self.width = width if width else length
+
+    def perimeter(self):
+        return self.length * 2 + self.width * 2
+
+    def area_rectangle(self):
+        return self.length * self.width
+
+
+rect1 = Rectangle(5, 10)
+print(rect1.perimeter())
+print(rect1.area_rectangle())
