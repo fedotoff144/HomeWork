e956d3c851b27d12fa77abdeb9d672250c377f64
fedotoff144 PB laptop <fedotoff144@gmail.com>
2024-05-16 09:30:46 +0300

    Task_05 solved

--- Dive into python/Lesson_11.1/Task_05.py
+++ Dive into python/Lesson_11.1/Task_05.py
@@ -4,7 +4,7 @@
 - Добавьте возможность сложения и вычитания.
 - При этом должен создаваться новый экземпляр
 прямоугольника.
-- Складываем и вычитаем периметры, а не длинну и ширину.
+- Складываем и вычитаем периметры, а не длину и ширину.
 - При вычитании не допускайте отрицательных значений.
 """
 
@@ -15,12 +15,34 @@ class Rectangle:
         self.width = width if width else length
 
     def perimeter(self):
-        return self.length * 2 + self.width * 2
+        return (self.length + self.width) * 2
 
     def area_rectangle(self):
         return self.length * self.width
 
+    # def __add__(self, other):
+    #     length = self.length + other.length
+    #     width = self.width + other.width
+    #     return Rectangle(length, width)
+
+    def __add__(self, other):
+        return self.perimeter() + other.perimeter()
+
+    # def __sub__(self, other):
+    #     length = self.length - other.length
+    #     width = self.width - other.width
+    #     if length < 0 or width < 0:
+    #         return 'Minus is not allowed'
+    #     return Rectangle(length, width)
+
+    def __sub__(self, other):
+        result = self.perimeter() - other.perimeter()
+        if result < 0:
+            return 'Minus is not allowed'
+        return result
+
 
 rect1 = Rectangle(5, 10)
-print(rect1.perimeter())
-print(rect1.area_rectangle())
+rect2 = Rectangle(5, 10)
+print(rect1 + rect2)
+print(rect1 - rect2)
