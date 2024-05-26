56e09f26761b83ce1acd27368180912effa3bc55
fedotoff144 GigabyteLaptop <fedotoff144@gmail.com>
2024-05-26 22:13:03 +0300

    Lesson_02.1 Task_2 solved

--- Dive into python/Lesson_02.1/Task_02.py
+++ Dive into python/Lesson_02.1/Task_02.py
@@ -18,3 +18,26 @@ frac2 = "1/3"
 Сумма дробей: 5/6
 Произведение дробей: 1/6
 """
+import fractions
+
+frac1 = "5/2"
+frac2 = "1/3"
+
+numerator_frac1 = int(frac1.split('/')[0])
+denominator_frac1 = int(frac1.split('/')[1])
+numerator_frac2 = int(frac2.split('/')[0])
+denominator_frac2 = int(frac2.split('/')[1])
+
+# sum fractions
+denominator_sum = denominator_frac1 * denominator_frac2
+numerator_sum = denominator_frac1 * numerator_frac2 + denominator_frac2 * numerator_frac1
+result_sum = f'{numerator_sum}/{denominator_sum}'
+
+# mult fractions
+denominator_mult = denominator_frac1 * denominator_frac2
+numerator_mult = numerator_frac1 * numerator_frac2
+result_mult = f'{numerator_mult}/{denominator_mult}'
+
+print(f'Сумма дробей: {result_sum}\nПроизведение дробей: {result_mult}')
+print(f'Сумма дробей: {fractions.Fraction(frac1) + fractions.Fraction(frac2)}\n'
+      f'Произведение дробей: {fractions.Fraction(frac1) * fractions.Fraction(frac2)}')
