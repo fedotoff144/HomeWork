ec829069f7d61fe299a8c2f66ffe95a25287911d
fedotoff144 GigabyteLaptop <fedotoff144@gmail.com>
2024-05-29 23:25:09 +0300

    Dive into python Lesson_03.1 Task_01 solved

--- Dive into python/Lesson_03.1/Task_01.py
+++ Dive into python/Lesson_03.1/Task_01.py
@@ -21,3 +21,26 @@ max_weight = 1.0
 На выходе, например, один из допустимых вариантов может быть таким:
 {'ключи': 0.3, 'кошелек': 0.2, 'зажигалка': 0.1}
 """
+items = {
+    "ключи": 0.3,
+    "кошелек": 0.2,
+    "телефон": 0.5,
+    "зажигалка": 0.1
+}
+MAX_WEIGHT = 1.0
+result_lst = []
+copy_items = items.copy()
+
+for key in items.keys():
+    check_weight = MAX_WEIGHT
+    temp_dict = {}
+
+    for sec_key, sec_value in copy_items.items():
+        if check_weight - copy_items[sec_key] >= 0:
+            temp_dict[sec_key] = sec_value
+            check_weight -= copy_items[sec_key]
+    copy_items.pop(key)
+    result_lst.append(temp_dict)
+
+for i in result_lst:
+    print(i)
