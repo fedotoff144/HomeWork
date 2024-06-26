576d876c654d994d0b22da6c803d1124215fe27b
fedotoff144 GigabyteLaptop <fedotoff144@gmail.com>
2024-06-26 14:08:46 +0300

    Dive into python Lesson_10.1 task_12 solved

--- Dive into python/Lesson_10.1/task_12.py
+++ Dive into python/Lesson_10.1/task_12.py
@@ -21,3 +21,28 @@ matching_numbers = game.compare_lists()
 Совпадающие числа: [3, 12, 8, 41, 9, 14, 5]
 Количество совпадающих чисел: 7
 """
+
+
+class LotteryGame:
+    def __init__(self, list1, list2):
+        self.list1 = list1
+        self.list2 = list2
+
+    def compare_lists(self):
+        lst_win_nums = []
+        for i in self.list1:
+            if i in self.list2:
+                lst_win_nums.append(i)
+                self.list2.remove(i)
+        if len(lst_win_nums) > 0:
+            print(f'Совпадающие числа: {lst_win_nums}\n'
+                  f'Количество совпадающих чисел: {len(lst_win_nums)}')
+        else:
+            print('Совпадающих чисел нет.')
+
+
+if __name__ == '__main__':
+    list1 = [3, 12, 8, 41, 7, 21, 9, 14, 5, 30]
+    list2 = [9, 5, 6, 12, 14, 22, 17, 41, 8, 3]
+    game = LotteryGame(list1, list2)
+    game.compare_lists()
