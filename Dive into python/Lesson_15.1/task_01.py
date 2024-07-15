cacd4a21bd9928fe9a012671b3ae6ef5fa70b98f
fedotoff144 GigabyteLaptop <fedotoff144@gmail.com>
2024-07-15 22:54:39 +0300

    Dive into python Lesson_15.1 task_1 in process of solving

--- Dive into python/Lesson_15.1/task_01.py
+++ Dive into python/Lesson_15.1/task_01.py
@@ -8,46 +8,46 @@ import json
 from typing import Callable
 import os
 
-FORMAT = '{levelname:<8} - {asctime}. В модуле "{name}" ' \
-         'в строке {lineno:03d} функция "{funcName}()" ' \
-         'в {created} секунд записала сообщение: {msg}'
-logging.basicConfig(format=FORMAT, style='{',
-                    level=logging.INFO,
-                    filemode='a',
-                    filename='log_task_01.log',
-                    encoding='utf-8'
-                    )
-logger = logging.getLogger(__name__)
-
-
-def my_deco(func: Callable):
-    def wrapper(args):
-        try:
-            result = func(args)
-            result_dict = {'function name': func.__name__,
-                           'function arguments': [*args],
-                           'result': result
-                           }
-
-            data = []
-            for cur_dir, dirs, files in os.walk(os.getcwd()):
-                if 'task_01_info.json' in files:
-                    with open('task_01_info.json', 'r+', encoding='utf-8') as f_json:
-                        data = json.load(f_json)
-
-            data.append(result_dict)
-            with open('task_01_info.json', 'w', encoding='utf-8') as f_json_write:
-                json.dump(data, f_json_write)
-            logger.info(result_dict)
-        except Exception as e:
-            logger.error(e)
-
-        return None
-
-    return wrapper
-
-
-@my_deco
+# FORMAT = '{levelname:<8} - {asctime}. В модуле "{name}" ' \
+#          'в строке {lineno:03d} функция "{funcName}()" ' \
+#          'в {created} секунд записала сообщение: {msg}'
+# logging.basicConfig(format=FORMAT, style='{',
+#                     level=logging.INFO,
+#                     filemode='a',
+#                     filename='log_task_01.log',
+#                     encoding='utf-8'
+#                     )
+# logger = logging.getLogger(__name__)
+#
+#
+# def my_deco(func: Callable):
+#     def wrapper(args):
+#         try:
+#             result = func(args)
+#             result_dict = {'function name': func.__name__,
+#                            'function arguments': [*args],
+#                            'result': result
+#                            }
+#
+#             data = []
+#             for cur_dir, dirs, files in os.walk(os.getcwd()):
+#                 if 'task_01_info.json' in files:
+#                     with open('task_01_info.json', 'r+', encoding='utf-8') as f_json:
+#                         data = json.load(f_json)
+#
+#             data.append(result_dict)
+#             with open('task_01_info.json', 'w', encoding='utf-8') as f_json_write:
+#                 json.dump(data, f_json_write)
+#             logger.info(result_dict)
+#         except Exception as e:
+#             logger.error(e)
+#
+#         return None
+#
+#     return wrapper
+#
+#
+# @my_deco
 def transpose(matrix):
     if len(matrix) == 1:
         return matrix
