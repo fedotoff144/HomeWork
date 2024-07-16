3f71cfb7f628984c3304a2bf46a916d9365a6dfa
fedotoff144 TecnoLaptop <fedotoff144@gmail.com>
2024-07-16 22:55:48 +0300

    Dive into python Lesson_15 task_01 solved

--- Dive into python/Lesson_15.1/task_01.py
+++ Dive into python/Lesson_15.1/task_01.py
@@ -4,75 +4,45 @@
 """
 import logging
 import argparse
-import json
-from typing import Callable
-import os
+import ast
 
-# FORMAT = '{levelname:<8} - {asctime}. В модуле "{name}" ' \
-#          'в строке {lineno:03d} функция "{funcName}()" ' \
-#          'в {created} секунд записала сообщение: {msg}'
-# logging.basicConfig(format=FORMAT, style='{',
-#                     level=logging.INFO,
-#                     filemode='a',
-#                     filename='log_task_01.log',
-#                     encoding='utf-8'
-#                     )
-# logger = logging.getLogger(__name__)
-#
-#
-# def my_deco(func: Callable):
-#     def wrapper(args):
-#         try:
-#             result = func(args)
-#             result_dict = {'function name': func.__name__,
-#                            'function arguments': [*args],
-#                            'result': result
-#                            }
-#
-#             data = []
-#             for cur_dir, dirs, files in os.walk(os.getcwd()):
-#                 if 'task_01_info.json' in files:
-#                     with open('task_01_info.json', 'r+', encoding='utf-8') as f_json:
-#                         data = json.load(f_json)
-#
-#             data.append(result_dict)
-#             with open('task_01_info.json', 'w', encoding='utf-8') as f_json_write:
-#                 json.dump(data, f_json_write)
-#             logger.info(result_dict)
-#         except Exception as e:
-#             logger.error(e)
-#
-#         return None
-#
-#     return wrapper
-#
-#
-# @my_deco
+
+FORMAT = '{levelname:<8} - {asctime}. В модуле "{name}" ' \
+         'в строке {lineno:03d} запуск функции "{funcName}()". ' \
+         'Статус работы: {msg}'
+logging.basicConfig(format=FORMAT, style='{', level=logging.DEBUG,
+                    filename='log_task_01.log', filemode='a', encoding='utf-8')
+logger = logging.getLogger('task_1.py')
+
+
 def transpose(matrix):
     if len(matrix) == 1:
         return matrix
+
     result = []
-    length_element = len(matrix[1])
-    for i in range(length_element):
-        temp_lst = [line[i] for line in matrix]
+    length_row = len(matrix[0])
+    for i in range(length_row):
+        temp_lst = [row[i] for row in matrix]
         result.append(temp_lst)
+
     return result
 
 
 def parse():
     parser = argparse.ArgumentParser(description='Task for intermediate certification',
-                                     prog='transpose()',
-                                     epilog='End of help')
-    parser.add_argument('-m', '--matrix', default='[[1,2,3],[1,2,3],[1,2,3]]',
-                        help='pass a list of format [[1,2,3],[1,2,3],[1,2,3]]')
-    args = parser.parse_args()
-    return transpose(args)
+                                     prog='transpose', epilog='Returns the transposed matrix')
+    try:
+        parser.add_argument('-m', '--matrix', metavar='MATRIX', type=str, nargs=1,
+                            default='[[1,2,3],[1,2,3],[1,2,3]]',
+                            help='pass matrix as a string without spaces, for example: [[1,2,3],[1,2,3],[1,2,3]]')
+        args = parser.parse_args()
+        matrix = ast.literal_eval(*args.matrix)
+        result = transpose(matrix)
+        logger.info('OK')
+        return result
+    except Exception as e:
+        logger.error(f'ERROR {e}')
 
 
 if __name__ == '__main__':
-    # print('ok')
-    # matrix = [[11, 21, 31],
-    #           [41, 51, 61],
-    #           [71, 81, 91]]
-    # new_matrix = transpose(matrix)
-    parse()
+    print('OK...' if parse() else 'ERROR!')
